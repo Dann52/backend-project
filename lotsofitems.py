e3ff6f653a9395c5d3b47d0abe3e12399b2e53e3
Dann52 <danvieira2@gmail.com>
2018-07-24 19:45:25 -0400

    Added changes from remote master. Changes from lightsail/ubuntu server.

--- lotsofitems.py
+++ lotsofitems.py
@@ -5,7 +5,7 @@ from database_setup import PlaceCategory, Base, Place, User
 
 # -*- coding: utf-8 -*-
  
-engine = create_engine('sqlite:///places.db')
+engine = create_engine('postgresql://catalog:catalog@localhost:5432/catalogdb')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
